93e0f27e82291a366d738922b8994548894f2c58
Noe <noe@gmail.com>
2022-09-27 18:56:14 -0500

    Agregar archivos de proyecto.

---
+++ LabS5/LabS5/ImageResourceExtension.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Reflection;
+using Xamarin.Forms.Xaml;
+using Xamarin.Forms;
+using Xamarin.Forms.Internals;
+using System.Runtime.InteropServices;
+
+namespace LabS5
+{
+    [Preserve(AllMembers = true)]
+    [ContentProperty(nameof(Source))]
+    public class ImageResourceExtension : IMarkupExtension
+    {
+        public string Source { get; set; }
+        public object ProvideValue(IServiceProvider serviceProvider)
+        {
+            if (Source == null)
+                return null;
+            // Do your translation lookup here, using whatever method you
+            var imageSource = ImageSource.FromResource(Source,
+                typeof(ImageResourceExtension).GetTypeInfo().Assembly);
+            return imageSource;
+        }
+    }
+}
